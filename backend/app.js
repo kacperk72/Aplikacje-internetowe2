feace2cb1214fbd4fa03b0fec94b103421ffcf26
Kacper Kubit <kacper.kubit99@gmail.com>
2023-07-24 17:34:25 +0200

    register doctor

--- backend/app.js
+++ backend/app.js
@@ -1,99 +1,103 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const cors = require('cors');
-const mysql = require('mysql2/promise');
+const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'med_app'
+  host: "localhost",
+  user: "root",
+  database: "med_app",
 });
 
-app.get('/api/doctors', async (req, res) => {
-    try {
-        const [rows] = await pool.query(
-            `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
+app.get("/api/doctors", async (req, res) => {
+  try {
+    const [rows] = await pool.query(
+      `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
         FROM users
         JOIN doctors ON users.id = doctors.user_id`
-        );
-        res.json(rows);
-    } catch (error) {
-        console.error('Error fetching doctors:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+    );
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching doctors:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-app.get('/api/doctor/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const [[doctor]] = await pool.query(
-            `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
+app.get("/api/doctor/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const [[doctor]] = await pool.query(
+      `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
             FROM users
             JOIN doctors ON users.id = doctors.user_id
-            WHERE users.id = ?`, [id]
-        );
+            WHERE users.id = ?`,
+      [id]
+    );
 
-        const [[{ avg_mark }]] = await pool.query(
-            'SELECT AVG(mark) as avg_mark FROM comments WHERE doctor_id = ?', [id]
-        );
+    const [[{ avg_mark }]] = await pool.query(
+      "SELECT AVG(mark) as avg_mark FROM comments WHERE doctor_id = ?",
+      [id]
+    );
 
-        if(doctor) {
-            doctor.avg_mark = parseFloat(avg_mark).toFixed(2);
-            res.json(doctor);
-        } else {
-            res.status(404).send('Doctor not found');
-        }
-    } catch (error) {
-        console.error('Error fetching doctor:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (doctor) {
+      doctor.avg_mark = parseFloat(avg_mark).toFixed(2);
+      res.json(doctor);
+    } else {
+      res.status(404).send("Doctor not found");
     }
+  } catch (error) {
+    console.error("Error fetching doctor:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
+app.get("/api/doctors/search", async (req, res) => {
+  const { query } = req.query;
 
-app.get('/api/doctors/search', async (req, res) => {
-    const { query } = req.query;
-
-    try {
-        const [rows] = await pool.query(
-            `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
+  try {
+    const [rows] = await pool.query(
+      `SELECT users.id as user_id, users.name, users.surname, doctors.speciality, doctors.localization
             FROM users
             JOIN doctors ON users.id = doctors.user_id
             WHERE users.name LIKE ? OR users.surname LIKE ?`,
-            [`%${query}%`, `%${query}%`]
-        );
-        res.json(rows);
-    } catch (error) {
-        console.error('Error fetching doctors:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+      [`%${query}%`, `%${query}%`]
+    );
+    res.json(rows);
+  } catch (error) {
+    console.error("Error fetching doctors:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-
-app.get('/api/reviews/:doctorId', async (req, res) => {
-    const { doctorId } = req.params;
-    try {
-        const [reviews] = await pool.query(`
+app.get("/api/reviews/:doctorId", async (req, res) => {
+  const { doctorId } = req.params;
+  try {
+    const [reviews] = await pool.query(
+      `
             SELECT comments.*, users.name as author_name, users.surname as author_surname 
             FROM comments 
             JOIN users ON comments.user_id = users.id 
             WHERE doctor_id = ?
-        `, [doctorId]);
-        res.send(reviews);
-    } catch (error) {
-        console.error('Error fetching reviews:', error);
-        res.status(500).send('Server error');
-    }
+        `,
+      [doctorId]
+    );
+    res.send(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).send("Server error");
+  }
 });
 
-app.get('/api/reviewsByUser/:userId', async (req, res) => {
-    const { userId } = req.params;
-    try {
-        const [reviews] = await pool.query(`
+app.get("/api/reviewsByUser/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const [reviews] = await pool.query(
+      `
             SELECT comments.*, u.name as author_name, u.surname as author_surname,
             d.name as doctor_name, d.surname as doctor_surname
             FROM comments 
@@ -101,73 +105,91 @@ app.get('/api/reviewsByUser/:userId', async (req, res) => {
             JOIN doctors ON comments.doctor_id = doctors.user_id
             JOIN users d ON doctors.user_id = d.id
             WHERE comments.user_id = ?
-        `, [userId]);
-        res.send(reviews);
-    } catch (error) {
-        console.error('Error fetching reviews:', error);
-        res.status(500).send('Server error');
-    }
+        `,
+      [userId]
+    );
+    res.send(reviews);
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    res.status(500).send("Server error");
+  }
 });
 
-app.post('/api/reviews', async (req, res) => {
-    const { userId, doctorId, mark, comment } = req.body;
-    try {
-        await pool.query(
-            'INSERT INTO comments (doctor_id, user_id, comment, mark) VALUES (?, ?, ?, ?)',
-            [doctorId, userId, comment, mark]
-        );
-        res.send('Review submitted successfully');
-    } catch (error) {
-        console.error('Error submitting review:', error);
-        res.status(500).send('Server error');
-    }
+app.post("/api/reviews", async (req, res) => {
+  const { userId, doctorId, mark, comment } = req.body;
+  try {
+    await pool.query(
+      "INSERT INTO comments (doctor_id, user_id, comment, mark) VALUES (?, ?, ?, ?)",
+      [doctorId, userId, comment, mark]
+    );
+    res.send("Review submitted successfully");
+  } catch (error) {
+    console.error("Error submitting review:", error);
+    res.status(500).send("Server error");
+  }
 });
 
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const name = req.body.name;
+  const surname = req.body.surname;
+  const isDoctor = req.body.isDoctor;
+  const specialization = req.body.specialization;
+  const city = req.body.city;
 
-app.post('/register', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    const name = req.body.name;
-    const surname = req.body.surname;
-    const isDoctor = req.body.isDoctor;
+  try {
+    await pool.query(
+      "INSERT INTO users (name, surname, login, password, isDoctor) VALUES (?, ?, ?, ?, ?)",
+      [name, surname, username, password, isDoctor]
+    );
 
-    try {
-        await pool.query(
-            'INSERT INTO users (name, surname, login, password, isDoctor) VALUES (?, ?, ?, ?, ?)',
-            [name, surname, username, password, isDoctor]
-        );
-        res.send('User registered successfully');
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server error');
+    const [user] = await pool.query(
+      "SELECT * FROM users WHERE login = ? AND password = ?",
+      [username, password]
+    );
+
+    if (user[0].isDoctor === 1) {
+      await pool.query(
+        "INSERT INTO doctors (user_id, speciality, localization) VALUES (?, ?, ?)",
+        [user[0].id, specialization, city]
+      );
     }
+
+    res.send("User registered successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
 });
 
-app.post('/login', async (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-    try {
-        const [results] = await pool.query('SELECT * FROM users WHERE login = ?', [username]);
-        if (results.length > 0) {
-            const user = results[0];
-            if(password == user.password) {
-                res.json({
-                    message: 'Logged in successfully',
-                    user: user
-                });
-            } else {
-                res.send('Wrong username/password combination');
-            }
-        } else {
-            res.send('User does not exist');
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server error');
+  try {
+    const [results] = await pool.query("SELECT * FROM users WHERE login = ?", [
+      username,
+    ]);
+    if (results.length > 0) {
+      const user = results[0];
+      if (password == user.password) {
+        res.json({
+          message: "Logged in successfully",
+          user: user,
+        });
+      } else {
+        res.send("Wrong username/password combination");
+      }
+    } else {
+      res.send("User does not exist");
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
 });
 
-app.listen(3000, function() {
-    console.log('Server is running on port 3000');
+app.listen(3000, function () {
+  console.log("Server is running on port 3000");
 });
--- frontend/med-app/src/register/register.tsx
+++ frontend/med-app/src/register/register.tsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
   const [isDoctor, setIsDoctor] = useState(false);
+  const [specialization, setSpecialization] = useState(""); // Dodane pole "Specjalizacja"
+  const [city, setCity] = useState(""); // Dodane pole "Miasto"
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3000/register', {
+      const response = await axios.post("http://localhost:3000/register", {
         username,
         password,
         name,
         surname,
-        isDoctor
+        isDoctor,
+        specialization,
+        city,
       });
       alert(response.data);
       window.location.reload();
@@ -26,49 +30,71 @@ function Register() {
   };
 
   return (
-    <div className='registerForm'>
-    <form onSubmit={handleSubmit}>
-      <h1>Rejestracja</h1>
-      <input
-        type="text"
-        placeholder="Imie"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Nazwisko"
-        value={surname}
-        onChange={e => setSurname(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Login"
-        value={username}
-        onChange={e => setUsername(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Hasło"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        required
-      />
-      <label>
+    <div className="registerForm">
+      <form onSubmit={handleSubmit}>
+        <h1>Rejestracja</h1>
         <input
-          type="checkbox"
-          checked={isDoctor}
-          className='checkboxInput'
-          onChange={e => setIsDoctor(e.target.checked)}
+          type="text"
+          placeholder="Imie"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
         />
-        Jesteś lekarzem?
-      </label>
-      <br></br>
-      <button type="submit" className='registerButton'>Register</button>
-    </form>
+        <input
+          type="text"
+          placeholder="Nazwisko"
+          value={surname}
+          onChange={(e) => setSurname(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Login"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Hasło"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <label>
+          <input
+            type="checkbox"
+            checked={isDoctor}
+            className="checkboxInput"
+            onChange={(e) => setIsDoctor(e.target.checked)}
+          />
+          Jesteś lekarzem?
+        </label>
+
+        {isDoctor && (
+          <>
+            <input
+              type="text"
+              placeholder="Specjalizacja"
+              value={specialization}
+              onChange={(e) => setSpecialization(e.target.value)}
+              required
+            />
+            <input
+              type="text"
+              placeholder="Miasto"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            />
+          </>
+        )}
+
+        <br></br>
+        <button type="submit" className="registerButton">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
